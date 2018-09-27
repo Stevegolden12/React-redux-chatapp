ef56b660270e5f0277acb379ff16a4bf73dd447e
Steve Golden <sgolden2525@outlook.com>
2018-09-27 12:54:31 -0600

    Server functionality and dispatching action
    We dispatched adduser from actions/index.js
    
    Added Server functionality and started listening for user and message events.
    
    Added Server folder/app.js file.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore } from 'redux
+import { addUser } from './actions';
 
 import chat from './reducers';
 
 const store = createStore(chat);
+
+store.dispatch(addUser('Me'))
 
 ReactDOM.render(
   <Provider store={store} />
